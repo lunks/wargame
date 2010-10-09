76f24be5b0937e7caef0f55d205d054b8958d48d
Pedro Nascimento <pnascimento@gmail.com>
2010-10-09 17:35:33 -0300

    buying ships logic

--- app/models/squad.rb
+++ app/models/squad.rb
@@ -1,5 +1,12 @@
 class Squad < ActiveRecord::Base
   has_many :planets
   has_and_belongs_to_many :ships
+  def buy ship
+    if self.ships.include? ship
+      self.credits = self.credits - ship.price
+    else
+      false
+    end
+  end
 end
 
--- spec/models/squad_spec.rb
+++ spec/models/squad_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
 describe Squad do
+  let(:squad) {Factory :squad}
   it {should have_many :planets}
   it {should have_and_belong_to_many :ships }
+  context 'buying and selling ships' do
+    before(:each) do
+      @ship = Factory :ship
+      squad.ships << @ship
+    end
+    it 'should be able to buy ships and remove credits accordingly' do
+      squad.credits = 1000
+      @ship.price = 1000
+     # squad.ships.push ship
+      squad.buy @ship
+      squad.credits.should be_zero
+    end
+
+    it 'should not be able to buy a ship they dont have access to' do
+      squad.ships.clear
+      squad.buy(@ship).should be_false
+    end
+
+    it 'should be able to buy a ship they have access to' do
+      #squad.ships << ship
+      squad.buy(@ship).should be_true
+    end
+  end
+
 end
 
