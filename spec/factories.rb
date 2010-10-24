aca66fa960695fcfddd4f8089fe8cabaa05eb60b
Pedro Nascimento <pnascimento@gmail.com>
2010-10-24 17:54:30 -0200

    factories for Planet and OwnedShip

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,9 +2,20 @@ Factory.define :ship do |f|
   f.name Forgery::Name.full_name
   f.price Forgery::Basic.number
 end
+
 Factory.define :squad do |f|
   f.name Forgery::Name.full_name
   f.credits Forgery::Basic.number
   f.ships {|a| [a.association(:ship)] }
+end
+
+Factory.define :planet do |f|
+  f.name Forgery::Name.full_name
+end
+
+Factory.define :owned_ship do |f|
+  f.ship {|a| a.association(:ship) }
+  f.squad {|a| a.association(:squad) }
+  f.quantity Forgery::Basic.number
 end
 
