13c68690ba716f03922147cbdd822fc3dabcfe32
Pedro Nascimento <pnascimento@gmail.com>
2010-10-24 17:53:52 -0200

    planets can have ships on their orbit

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,5 @@
 class Planet < ActiveRecord::Base
   belongs_to :squad
+  has_many :owned_ships
 end
 
--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -2,5 +2,6 @@ require 'spec_helper'
 
 describe Planet do
   it {should belong_to :squad}
+  it {should have_many :owned_ships}
 end
 
