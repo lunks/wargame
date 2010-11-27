91e8be568f2dafb36995dd57ba7ef770a6c18f3b
Pedro Nascimento <pnascimento@gmail.com>
2010-11-27 21:16:23 -0200

    calculate credits

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,5 +1,13 @@
 class Planet < ActiveRecord::Base
   belongs_to :squad
   has_many :owned_ships
+
+  def credits_per_turn
+    if owned_ships.empty?
+      0
+    else
+      credits
+    end
+  end
 end
 
