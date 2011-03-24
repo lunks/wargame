edc184b9662fc1df68d70a19cda59c9ed225057b
Morali <rodrigo.morali@gmail.com>
2011-03-24 16:15:05 -0700

    showing fleets and facilities in partials

--- app/controllers/facility_fleets_controller.rb
+++ app/controllers/facility_fleets_controller.rb
@@ -1,1 +1,11 @@
-# developing
+class FacilityFleetsController < ApplicationController
+
+  def index
+    @units = Unit.allowed_for(current_squad)
+  end
+
+  def update_production_unit
+  end
+
+end
+
