23629389da132f5c7a6bf9009f39b949ca4db976
Pedro Nascimento <pnascimento@gmail.com>
2011-03-31 03:03:39 -0300

    acceptance group \o/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,12 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
+require 'capybara/rspec'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include AcceptanceExampleGroup, :example_group => { :file_path => /\bspec\/acceptance\// }
   config.mock_with :rspec
   config.fail_fast = true
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
