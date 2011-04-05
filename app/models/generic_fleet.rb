07ee11abf7ee5a3f33cba06336ede0b2e50c8114
Pedro Nascimento <pnascimento@gmail.com>
2011-04-05 20:54:12 -0300

    always destroys fleets if they are empty

--- app/models/generic_fleet.rb
+++ app/models/generic_fleet.rb
@@ -3,14 +3,15 @@ class GenericFleet < ActiveRecord::Base
   belongs_to :planet
   belongs_to :generic_unit
 
+  after_save :destroy_if_empty
+
   def captured! quantity, squad
     self.quantity = self.quantity - quantity
-    self.destroy if self.quantity == 0
-
     captured_fleet = self.clone
     captured_fleet.squad = squad
     captured_fleet.quantity = quantity
     captured_fleet.save
+    save
   end
 
   def type?(type)
@@ -19,6 +20,10 @@ class GenericFleet < ActiveRecord::Base
 
   def to_s
     "Type: #{generic_unit.name} Planet: #{planet.name} Quantity: #{self.quantity}"
+  end
+
+  def destroy_if_empty
+    destroy if self.quantity == 0
   end
 
 
