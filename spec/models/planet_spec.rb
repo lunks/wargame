41455c462f3a49b3c53bc765f8e29710dd42f48b
Rodrigo Morali <rodrigo.morali@gmail.com>
2011-04-20 15:14:03 -0700

    ok, we got a bug on seen_by

--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -19,6 +19,11 @@ describe Planet do
       not_seen = Factory :planet
       Planet.seen_by(@squad).should_not include not_seen
     end
+    pending 'should not bring more than one instance of the planet' do
+      Factory :generic_fleet, :planet => planet, :squad => @squad
+      planets = Planet.seen_by(@squad)
+      planets.should == planets.uniq
+    end
   end
 
   it 'should output its profits if the squad has air and ground ownership' do
