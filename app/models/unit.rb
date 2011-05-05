a844b04a890f22e141708f6284c31f2c8b2ceb93
Morali <rodrigo.morali@gmail.com>
2011-05-05 18:12:38 -0300

    Fixed querying Units

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,8 @@
 class Unit < GenericUnit
+  require 'fighter'
+  require 'light_transport'
+  require 'capital_ship'
+  require 'armament'
+  require 'trooper'
   scope :fighter, where(:price => 200...500)
 end
