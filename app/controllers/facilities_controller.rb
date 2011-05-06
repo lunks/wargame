721817788ea5d37c01fb39f39e70f75c69878efc
Morali <rodrigo.morali@gmail.com>
2011-05-06 19:50:15 -0300

    Implemented buy facilities

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -5,4 +5,13 @@ class FacilitiesController < ApplicationController
     @facilities = Facility.allowed_for(current_squad.faction)
   end
 
+  def buy_facility
+    @facility = Facility.find(params[:buy_facility][:id])
+    @planet = Planet.find(params[:buy_facility][:planet])
+    if current_squad.credits >= @facility.price
+      current_squad.buy @facility, 1, @planet
+    end
+    redirect_to :fleets
+  end
+
 end
