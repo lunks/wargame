ecca0b8f8b72fe398d08bbe8f9a9c840a88ed4e1
Morali <rodrigo.morali@gmail.com>
2011-05-05 21:49:02 -0300

    Puting new game scopes capital_ship and fighter on unit

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,4 +1,4 @@
 class Fighter < Unit
-  scope :fighter, where(:price => 60...400)
+
 end
 
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -4,5 +4,6 @@ class Unit < GenericUnit
   require 'capital_ship'
   require 'armament'
   require 'trooper'
-  scope :fighter, where(:price => 200...500)
+  scope :fighter, where(:price => 60..150)
+  scope :capital_ship, where(:price => 500..1000)
 end
