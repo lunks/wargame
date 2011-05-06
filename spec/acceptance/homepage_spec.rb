400b853b1612403ebf558121deeca5f7f364197d
Morali <rodrigo.morali@gmail.com>
2011-05-06 15:18:27 -0300

    Added randomized troopers

--- spec/acceptance/homepage_spec.rb
+++ spec/acceptance/homepage_spec.rb
@@ -5,7 +5,7 @@ describe 'homepage' do
   context 'not authenticated user' do
     it 'should see the homepage' do
       visit '/'
-      page.should have_content('Hello')
+      page.should have_content('Home / Index')
     end
     it 'should show me a login page to access a restricted area' do
       visit '/home/restricted'
