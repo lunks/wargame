5cab0b7c7727ce1c9817e8ef02e20956041ae809
Morali <rodrigo.morali@gmail.com>
2011-06-10 18:07:03 -0300

    Fixed devise sessions bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rpg::Application.config.session_store :cookie_store, :key => '_rpg_session'
+Rpg::Application.config.session_store :cookie_store, :key => '_wargame_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
